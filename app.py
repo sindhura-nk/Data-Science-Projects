976d94e5850d9c5ac55f317dd89f8dcb2ec60c40
sindhura-nk <nk.sindhura@gmail.com>
2024-06-25 20:06:54 +0530

    streamlit app py added

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import pickle
 import pandas as pd
 import numpy as np
 import re
-from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf1 =  TfidfVectorizer()
 
 # Build the user interface
 st.set_page_config(page_title='Restaurant Review',layout='wide')
@@ -19,9 +17,19 @@ review_input = st.text_input("Review: ")
 # Add a button to predict
 submit = st.button("Predict whether the review is positive or negative")
 
-# Load the pickle files: model files
-with open("Restaurant Review\model.pkl","rb") as file1:
-    model = pickle.load(file1)
+# Load the pickle files: tfidf file and model files. Using try except block to avoid errors
+try:
+    # Loading tfidf vectorizer
+    with open("Restaurant Review\mtf.pkl","rb") as file1:
+        tfidf = pickle.load(file1)
+    # Loading neural network model
+    with open("Restaurant Review\model.pkl","rb") as file2:
+        model = pickle.load(file2)
+except FileNotFoundError:
+    print(f"Error: File not found.")
+except (pickle.PickleError, AttributeError, TypeError) as e:
+    print(f"Error loading pickle file: {e}")    
+
 
 # Logic for prediction
 pattern = r'[^a-z\s]'
@@ -30,9 +38,9 @@ if submit:
     # convert the data before feeding to the model
     review_input = review_input.lower()
     review_input = re.sub(pattern,'',review_input)
-    X_new = tfidf1.transform([review_input]).toarray()
+    X_new = tfidf.transform([review_input]).toarray()
     probs1 = model.predict(X_new)
     if probs1>=0.5:
-        print('The given review is Positive')
+        st.subheader("The given review is Positive ")
     else:
-        print('The given review is Negative')
+        st.subheader("The given review is Negative ")
